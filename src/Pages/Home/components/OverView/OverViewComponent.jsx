1eb0ae333192840c03cb0c82b8e4c1d1043ca571
abdallaFoad <foadabdalla314@gmail.com>
2023-01-23 21:44:50 +0200

    add title

--- src/Pages/Home/components/OverView/OverViewComponent.jsx
+++ src/Pages/Home/components/OverView/OverViewComponent.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Box, OverViewContainer } from './style'
-import Container from 'react-bootstrap/Container';
-import dataOverView from 'yourStyled/components/overView/dataOverView';
-import OverView from 'yourStyled/components/overView/overView';
-
+import React from "react";
+import { Box, OverViewContainer } from "./style";
+import Container from "react-bootstrap/Container";
+import dataOverView from "yourStyled/components/overView/dataOverView";
+import OverView from "yourStyled/components/overView/overView";
 
 export default function OverViewComponent() {
   return (
     <OverViewContainer>
       <Container>
         <Box>
-          {
-            dataOverView.map((e) => {
-              return (
-                <div className='box'>
-                <OverView icon={e.icon} title={e.title} description={e.description} key={e.id} />
+          {dataOverView.map((e) => {
+            return (
+              <div className="box">
+                <OverView
+                  icon={e.icon}
+                  title={e.title}
+                  description={e.description}
+                  key={e.id}
+                />
               </div>
-            )
-            })
-          }
+            );
+          })}
         </Box>
       </Container>
     </OverViewContainer>
-  )
+  );
 }
